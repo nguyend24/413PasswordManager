96211c046e4bc89dd194a4ded74ff45994103966
Dennis Nguyen <dennisdnguyen19@gmail.com>
2021-11-14 01:13:30 -0500

    Display an error for an invalid login attempt

--- src/PasswordVault.java
+++ src/PasswordVault.java
@@ -2,101 +2,34 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
-public class PasswordVault extends JFrame {
 
-    /**
-     * LoginPanel is the initial view of the app frame.
-     */
-    static class LoginPanel extends JPanel {
-        LoginPanel(PasswordVault frame) {
-            this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+public class PasswordVault {
 
-            /*
-            Username entry
-             */
-            JPanel username = new JPanel();
-            username.add(new JLabel("Username"));
-            JTextField usernameField = new JTextField(20);
-            username.add(usernameField);
-            this.add(username);
+    private final JFrame     frame;
+    private LoginPanel loginPanel;
+    private       JButton    addPasswordButton;
+    private       JButton    viewPasswordsButton;
+    private       JButton    deletePasswordButton;
+    private       JTextField enterUsernameTextField;
+    private       JTextField enterPasswordTextField;
+    private       JTextField nameTextField;
+    private       JTextField display;
+    private       JLabel     msg1;
+    private       JLabel     msg2;
 
-            this.add(Box.createRigidArea(new Dimension(0, 5)));
+    public PasswordVault() throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 
-            /*
-            Password entry
-             */
-            JPanel password = new JPanel();
-            password.add(new JLabel("Password"));
-            JPasswordField passwordField = new JPasswordField(20);
-            passwordField.addActionListener(e -> login(usernameField.getText(), new String(passwordField.getPassword()), frame));
-            password.add(passwordField);
+        frame = new JFrame();
+        frame.setTitle("Password Vault");
+        frame.setSize(600, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-            this.add(password);
+        loginPanel = new LoginPanel(this);
 
-            this.add(Box.createRigidArea(new Dimension(0, 20)));
-
-            /*
-            Login button
-             */
-            JButton login = new JButton("Login");
-            login.addActionListener(e -> login(usernameField.getText(), new String(passwordField.getPassword()), frame));
-            login.setAlignmentX(Component.CENTER_ALIGNMENT);
-            this.add(login);
-
-            /*
-            Ensures a constant spacing between the username, password, and login areas by adding invisible at the bottom of the window
-             */
-            this.add(new Box.Filler(
-                new Dimension(0, 0),
-                new Dimension(0, Short.MAX_VALUE),
-                new Dimension(0, Short.MAX_VALUE)));
-
-        }
-
-        private void login(String user, String masterKey, PasswordVault frame) {
-            Vault vault;
-            try {
-//                vault = frame.client.retrieveVault(user, masterKey);
-            } catch (Exception e) {
-
-            }
-            frame.setContentPane(new VaultPanel(user, masterKey, frame, null));
-            frame.revalidate();
-        }
-    }
-
-    static class VaultPanel extends JPanel {
-        VaultPanel(String user, String masterKey, PasswordVault frame, Vault vault) {
-            this.add(new JLabel(user));
-            this.add(new JLabel(masterKey));
-
-            JButton logout = new JButton("Logout");
-            logout.setAlignmentX(Component.CENTER_ALIGNMENT);
-            logout.addActionListener(e -> {
-                frame.setContentPane(new LoginPanel(frame));
-                frame.revalidate();
-            });
-            this.add(logout);
-        }
-    }
-
-    private JButton    addPasswordButton;
-    private JButton    viewPasswordsButton;
-    private JButton    deletePasswordButton;
-    private JTextField enterUsernameTextField;
-    private JTextField enterPasswordTextField;
-    private JTextField nameTextField;
-    private JTextField display;
-    private JLabel     msg1;
-    private JLabel     msg2;
-    JFrame frame = new JFrame("My First Gui");
-
-    VaultManager vaultManager;
-    Client       client;
-
-    public PasswordVault() {
-        this.setContentPane(new LoginPanel(this));
 //        vaultManager = new VaultManager();
 //        client = new Client(vaultManager);
 //        JPanel panel1 = new JPanel();
@@ -135,19 +68,37 @@ public class PasswordVault extends JFrame {
 //        panel1.add(deletePasswordButton);
 //        panel1.add(viewPasswordsButton);
 //        this.add(panel1);
+    }
 
+    public static void main(String[] args) throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        PasswordVault vault = new PasswordVault();
+        vault.start();
     }
 
     public void start() {
-        this.setTitle("Password Vault");
-        this.setSize(600, 400);
-        this.setVisible(true);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(loginPanel);
+        frame.setVisible(true);
     }
 
-    public static void main(String[] args) {
-        PasswordVault vault = new PasswordVault();
-        vault.start();
+    public void revalidate() {
+        frame.revalidate();
+    }
+
+    public void login(String user, String password) {
+        try {
+            Client client = new Client(user, password);
+            Vault vault = client.retrieveVault();
+
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
+            ex.printStackTrace();
+        } catch (UserDoesNotExistException ue) {
+            loginPanel.invalidLogin();
+        }
+    }
+
+    public void logout() {
+        loginPanel = new LoginPanel(this);
+        frame.revalidate();
     }
 
     class Listener implements ActionListener {
@@ -164,3 +115,103 @@ public class PasswordVault extends JFrame {
         }
     }
 }
+
+/**
+ * LoginPanel is the initial view of the app frame.
+ */
+class LoginPanel extends JPanel {
+    private final PasswordVault passwordVault;
+    LoginPanel(PasswordVault passwordVault) {
+        this.passwordVault = passwordVault;
+
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        addLoginFields();
+
+        /*
+        Ensures a constant spacing between the username, password, and login areas by adding invisible at the bottom of the window
+         */
+        this.add(new Box.Filler(
+            new Dimension(0, 0),
+            new Dimension(0, Short.MAX_VALUE),
+            new Dimension(0, Short.MAX_VALUE)));
+
+        passwordVault.revalidate();
+    }
+
+    public void invalidLogin() {
+        this.removeAll();
+
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        addLoginFields();
+
+        /*
+        Invalid login Text
+         */
+        JLabel invalidLogin = new JLabel("Invalid Login Attempt!");
+        invalidLogin.setAlignmentX(Component.CENTER_ALIGNMENT);
+        invalidLogin.setForeground(Color.RED);
+        this.add(invalidLogin);
+
+        /*
+        Ensures a constant spacing between the username, password, and login areas by adding invisible at the bottom of the window
+         */
+        this.add(new Box.Filler(
+            new Dimension(0, 0),
+            new Dimension(0, Short.MAX_VALUE),
+            new Dimension(0, Short.MAX_VALUE)));
+
+        passwordVault.revalidate();
+    }
+
+    private void addLoginFields() {
+        /*
+        Username entry
+         */
+        JPanel username = new JPanel();
+        username.add(new JLabel("Username"));
+        JTextField usernameField = new JTextField(20);
+        username.add(usernameField);
+        username.grabFocus();
+        this.add(username);
+
+        this.add(Box.createRigidArea(new Dimension(0, 5)));
+
+        /*
+        Password entry
+         */
+        JPanel password = new JPanel();
+        password.add(new JLabel("Password"));
+        JPasswordField passwordField = new JPasswordField(20);
+        passwordField.addActionListener(e -> passwordVault.login(usernameField.getText(), new String(passwordField.getPassword())));
+        password.add(passwordField);
+
+        this.add(password);
+
+        this.add(Box.createRigidArea(new Dimension(0, 20)));
+
+        /*
+        Login button
+         */
+        JButton login = new JButton("Login");
+        login.addActionListener(e -> passwordVault.login(usernameField.getText(), new String(passwordField.getPassword())));
+        login.setAlignmentX(Component.CENTER_ALIGNMENT);
+        this.add(login);
+    }
+}
+
+//class VaultPanel extends JPanel {
+//    VaultPanel(String user, String masterKey, Frame frame, Vault vault) {
+//        this.add(new JLabel(user));
+//        this.add(new JLabel(masterKey));
+//
+//        JButton logout = new JButton("Logout");
+//        logout.setAlignmentX(Component.CENTER_ALIGNMENT);
+//        logout.addActionListener(e -> {
+//            frame.setContentPane(new LoginPanel(frame));
+//            frame.revalidate();
+//        });
+//        this.add(logout);
+//    }
+//}
